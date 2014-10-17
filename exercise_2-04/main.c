0642a5003ae166f37d85f01a23bf11d94b03162f
Bill Kvasnikoff <wkvasnikoff@hotmail.com>
2014-10-16 21:46:03 -0700

    finish exercise_2-04

--- exercise_2-04/main.c
+++ exercise_2-04/main.c
@@ -10,17 +10,42 @@ void squeeze(char s[], int c)
 {
     int i, j;
     for (i = j = 0; s[i] != '\0'; i++)
-    if (s[i] != c)
-    s[j++] = s[i];
-    s[j] = '\0';
+        if (s[i] != c)
+            s[j++] = s[i];
+        s[j] = '\0';
 }
 */
 
-void squeeze(char s1[], char 
+void squeeze(char s1[], char s2[])
+{
+    int i, j, pos;
+    pos = 0;
+
+    for (i=0; s1[i] != '\0'; i++) {
+        for (j=0; s2[j] != '\0'; j++) {
+            if (s2[j] == s1[i]) {
+                j = -1;
+                break;
+            }
+        }
+        // not in squeeze list
+        if (j >= 0) {
+            s1[pos] = s1[i];
+            pos++;
+        }
+    }
+    s1[pos] = '\0';
+}
 
 int main()
 {
+    char s1[] = "squeeze";
+    char s2[] = "qe";
 
+    printf("initial: %s\n", s1);
+    printf("to remove: %s\n", s2);
+    squeeze(s1, s2);
+    printf("result: %s\n", s1);
 
     return 0;
 }
