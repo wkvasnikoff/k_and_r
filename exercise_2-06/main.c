76208136f840a4b424f2215dbdc44318e56bd611
Bill Kvasnikoff <wkvasnikoff@hotmail.com>
2014-10-17 07:25:01 -0700

    create skeleton for exercise_2-06

---
+++ exercise_2-06/main.c
@@ -0,0 +1,19 @@
+/*
+ * Exercise 2-6. Write a function setbits(x,p,n,y) that
+ * returns x with the n bits that begin at position
+ * p set to the rightmost n bits of y, leaving the other bits unchanged.
+ */
+
+#include <stdio.h>
+
+int setbits(int x, int p, int n, int y)
+{
+
+
+}
+
+int main()
+{
+
+    return 0;
+}
