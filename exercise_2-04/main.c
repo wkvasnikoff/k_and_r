67d843082da22ffe0946ae21ab49247ded79fbe0
Bill Kvasnikoff <wkvasnikoff@hotmail.com>
2014-10-16 19:38:30 -0700

    work on new exercise

---
+++ exercise_2-04/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes each character in s1 that
+ * matches any character in the string s2.
+ */
+/*
+/* squeeze: delete all c from s
+void squeeze(char s[], int c)
+{
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; i++)
+    if (s[i] != c)
+    s[j++] = s[i];
+    s[j] = '\0';
+}
+*/
+
+void squeeze(char s1[], char 
+
+int main()
+{
+
+
+    return 0;
+}
