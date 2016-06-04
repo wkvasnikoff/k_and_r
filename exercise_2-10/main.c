dee79794777cf736ef88dc8f86922f747b8c567a
Bill Kvasnikoff <wkvasnikoff@hotmail.com>
2016-06-03 17:23:24 -0700

    exercise 2-10

---
+++ exercise_2-10/main.c
@@ -0,0 +1,10 @@
+/*
+ * Exercise 2-10. Rewrite the function lower, which converts upper case letters to lower case,
+ *                with a conditional expression instead of if-else.
+ */
+
+
+int main()
+{
+
+}
